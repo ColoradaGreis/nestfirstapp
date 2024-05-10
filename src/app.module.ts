c4600043362cf81e22d0ec5178505174c0752700
ColoradaGreis <graciana.baratti@gmail.com>
2024-05-10 17:27:17 -0300

    Add ProjectController and TasksService to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TasksModule } from './tasks/tasks.module'
 import { ProjectsModule } from './projects/projects.module'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
+import { ProjectController } from './project/project.controller'
+import { TasksService } from './tasks/tasks.service'
 
 @Module({
   imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
-  controllers: [],
-  providers: []
+  controllers: [ProjectController],
+  providers: [TasksService]
 })
 export class AppModule {}
